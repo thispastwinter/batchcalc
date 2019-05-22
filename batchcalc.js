cbc1cdb8368b83f9067202e96e64629c1f0aff7a
Justin Klaas <klaas.justin@gmail.com>
2019-05-22 17:23:19 -0500

    implemented bitters and other spirit

--- batchcalc.js
+++ batchcalc.js
@@ -5,9 +5,11 @@ modalTrigger = () => {
   instance.open();
 }
 
-function Cocktail(spirit, liqueur, juice, syrup, water) {
+function Cocktail(spirit, other, liqueur, bitters, juice, syrup, water) {
   this.spirit = parseFloat(spirit);
+  this.other = parseFloat(other)
   this.liqueur = parseFloat(liqueur);
+  this.bitters = parseFloat(bitters);
   this.juice = parseFloat(juice);
   this.syrup = parseFloat(syrup);
   this.water = parseFloat(water);
@@ -38,16 +40,20 @@ document.addEventListener('click', function (event) {
   if (!event.target.matches('.btn')) return;
 
   let spirit = validate(batchCalc.elements['spirit'].value);
+  let otherSpirit = validate(batchCalc.elements['other'].value);
   let liqueur = validate(batchCalc.elements['liqueur'].value);
+  let bitters = validate(batchCalc.elements['bitters'].value);
   let juice = validate(batchCalc.elements['juice'].value);
   let syrup = validate(batchCalc.elements['syrup'].value);
   let water = validate(batchCalc.elements['water'].value);
 
-  let batchedCocktail = new Cocktail(spirit, liqueur, juice, syrup, water);
+  let batchedCocktail = new Cocktail(spirit, otherSpirit, liqueur, bitters, juice, syrup, water);
   console.log(batchedCocktail);
 
   let amount = batchCalc.elements['amount'].value;
-  let unit = batchCalc.elements['unit'].value;
+  let unit = document.getElementById('unit').value;
+  console.log(amount);
+  console.log(unit);
 
   switch (unit) {
     case 'gallons':
@@ -71,19 +77,27 @@ convertToMilliliters = (num, cocktail) => {
   console.log(total);
 
   let spirit = (cocktail.spirit / total * milliliters).toFixed(0);
+  let other = (cocktail.other / total * milliliters).toFixed(0);
   let liqueur = (cocktail.liqueur / total * milliliters).toFixed(0);
+  let bitters = (cocktail.bitters / total * milliliters).toFixed(2);
   let juice = (cocktail.juice / total * milliliters).toFixed(0);
   let syrup = (cocktail.syrup / total * milliliters).toFixed(0);
   let water = (cocktail.water / total * milliliters).toFixed(0);
 
+  console.log(bitters)
+
   const tdSpirit = document.getElementById('tdSpirit');
+  const tdOther = document.getElementById('tdOther');
   const tdLiqueur = document.getElementById('tdLiqueur');
+  const tdBitters = document.getElementById('tdBitters');
   const tdJuice = document.getElementById('tdJuice');
   const tdSyrup = document.getElementById('tdSyrup');
   const tdWater = document.getElementById('tdWater');
 
   tdSpirit.innerHTML = spirit + ' (ml)';
+  tdOther.innerHTML = other + ' (ml)';
   tdLiqueur.innerHTML = liqueur + ' (ml)';
+  tdBitters.innerHTML = bitters + ' (ml)';
   tdJuice.innerHTML = juice + ' (ml)';
   tdSyrup.innerHTML = syrup + ' (ml)';
   tdWater.innerHTML = water + ' (ml)';
@@ -98,20 +112,27 @@ convertToLiters = (num, cocktail) => {
 
   let spirit = Math.trunc((cocktail.spirit / total * milliliters) / 750);
   let spiritR = ((cocktail.spirit / total * milliliters) % 750).toFixed(0);
+  let other = Math.trunc((cocktail.other / total * milliliters) / 750);
+  let otherR = ((cocktail.other / total * milliliters) % 750).toFixed(0);
   let liqueur = Math.trunc((cocktail.liqueur / total * milliliters) / 750);
   let liqueurR = ((cocktail.liqueur / total * milliliters) % 750).toFixed(0);
+  let bitters = (cocktail.bitters / total * milliliters).toFixed(2);
   let juice = (cocktail.juice / total * milliliters).toFixed(0);
   let syrup = (cocktail.syrup / total * milliliters).toFixed(0);
   let water = (cocktail.water / total * milliliters).toFixed(0);
 
   const tdSpirit = document.getElementById('tdSpirit');
+  const tdOther = document.getElementById('tdOther');
   const tdLiqueur = document.getElementById('tdLiqueur');
+  const tdBitters = document.getElementById('tdBitters');
   const tdJuice = document.getElementById('tdJuice');
   const tdSyrup = document.getElementById('tdSyrup');
   const tdWater = document.getElementById('tdWater');
 
   tdSpirit.innerHTML = spirit + ' (btl) ' + spiritR + ' (ml)';
+  tdOther.innerHTML = other + ' (btl) ' + otherR + ' (ml)';
   tdLiqueur.innerHTML = liqueur + ' (btl) ' + liqueurR + ' (ml)';
+  tdBitters.innerHTML = bitters + ' (ml)';
   tdJuice.innerHTML = juice + ' (ml)';
   tdSyrup.innerHTML = syrup + ' (ml)';
   tdWater.innerHTML = water + ' (ml)';
